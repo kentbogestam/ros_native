ec896c139eadfb06357f32d6f8922927118997ac
gose123 <kaurm915@gmail.com>
2021-06-30 00:49:34 +0530

    30-June-Changes

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   View,
   Text,
@@ -10,19 +10,54 @@ import {
   TouchableOpacity,
   Image,
   Dimensions,
-  ScrollView,
+  ScrollView,PermissionsAndroid,
 } from 'react-native';
 import { WebView } from 'react-native-webview';
-
+import KeepAwake from 'react-native-keep-awake';
+import Permissions from "react-native-permissions";
 const screenWidth = Dimensions.get('screen').width;
-
+ const runFirst = `
+      window.isNativeApp = true;
+      true; // note: this is required, or you'll sometimes get silent failures
+    `;
 function App() {
+	 useEffect(async() => {
+	const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      {
+        title: "Audio Permission",
+    message:
+      "App needs access to your audio / microphone",
+    buttonNeutral: "Ask Me Later",
+    buttonNegative: "Cancel",
+    buttonPositive: "OK"
+      }
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log("You can use the camera");
+    } else {
+      console.log("Camera permission denied");
+    }
+    
+  });
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar backgroundColor="white" barStyle="dark-content" />
       <View style={styles.container}>
-        <WebView source={{ uri: 'https://ros-dev.dastjar.com/login' }} />
+        <WebView 
+		startInLoadingState={true} 
+		  originWhitelist={["*"]}
+		   mediaPlaybackRequiresUserAction={false}
+        domStorageEnabled={true}
+       allowsInlineMediaPlayback={true}    
+    javaScriptEnabled={ true } 
+	useWebKit={true}
+		source={{ uri: 'https://ros-dev.dastjar.com/' }}
+		 allowUniversalAccessFromFileURLs={true}
+		injectedJavaScriptBeforeContentLoaded={runFirst}
+		/>
       </View>
+	  <KeepAwake/>
     </SafeAreaView>
   );
 }
@@ -30,7 +65,7 @@ function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
+   
   }
   
 });
